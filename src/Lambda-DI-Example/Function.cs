7a24c4b45a96e8e5fcc9e176aa6ce741a61774e4
Brian Sheridan <brian.sheridan@gmail.com>
2020-01-22 19:57:18 -0800

    Added in Services

--- src/Lambda-DI-Example/Function.cs
+++ src/Lambda-DI-Example/Function.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using Microsoft.Extensions.DependencyInjection;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -12,13 +13,26 @@ namespace Lambda_DI_Example
 {
     public class Function
     {
+        private ServiceCollection _serviceCollection;
+        
         public Function()
         {
-            
+            ConfigureService();
         }
+
+        private void ConfigureService()
+        {
+            _serviceCollection = new ServiceCollection();
+            _serviceCollection.AddTransient<IMath,Math>();
+        }
+
         public string FunctionHandler(string input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            using (ServiceProvider serviceProvider = _serviceCollection.BuildServiceProvider())
+            {
+                // entry to run app.
+                return serviceProvider.GetService<Math>().Add(1,2).ToString();
+            }
         }
     }
 }
